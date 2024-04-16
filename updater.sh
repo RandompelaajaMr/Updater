c5be9ec8066d6627fef9ee4b2367722a0e52a1b7
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-16 19:16:58 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-echo testing, dont mind me
+FILES='/tmp/updater'
+mkdir /tmp/updater
+cd $FILES || exit 
+pwd 
+wget -q https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/ubuntu.sh 
+bash ./ubuntu.sh 
+sudo rm -rf /tmp/updater 
+cd ~ || exit

ea31959364bb4df583df0791b0ef30def51b7b34
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-16 18:29:35 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,4 +1,1 @@
-YAY, you got your first file downloaded from fancy pants github. 
-Be happy. 
-
-BYEEEEEEEEEEEEEEEE
+curl -sSL https://github.com/RandompelaajaMr/Updater/blob/ea9e5afa3b782f3ac5c356b484192c90b0f8ee26/ubuntu.sh | bash

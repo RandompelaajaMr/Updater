5806cfc1d36e1ef0faeb6065299822da251642cc
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-16 18:42:44 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,1 +1,1 @@
-( wget -O /tmp/updater/ubuntu.sh https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/ubuntu.sh && exec /tmp/updater/ubuntu.sh)
+wget -O /tmp/updater/ubuntu.sh https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/ubuntu.sh && exec /tmp/updater/ubuntu.sh

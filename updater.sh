794065d602668aa1d5a17b37e29baeb92883982b
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-16 18:31:38 +0300

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,1 +1,1 @@
-curl -sSL https://github.com/RandompelaajaMr/Updater/blob/ea9e5afa3b782f3ac5c356b484192c90b0f8ee26/ubuntu.sh | bash
+curl -sSL https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/ubuntu.sh | bash

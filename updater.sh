b8fedf441f8bc3a0289d6ce4190ef91407c5aefb
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-17 21:38:22 +0300

    Merging DEV -> main (#3)
    * Update updater.sh
    
    * Update README.md
    
    * Create ubuntu.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update ubuntu.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update updater.sh
    
    * Update ubuntu.sh
    
    * movinf files around
    
    * Adding/Modified files
    
    * Adding/Editing/Fixing files
    
    * Last edits
    
    * Fixed misstyped address

--- updater.sh
+++ updater.sh
@@ -1,1 +1,30 @@
+#!/bin/bash 
+source /etc/os-release
+mkdir -p /tmp/updater 
+FILES='/tmp/updater' 
+IDLIKE=$(awk '{print $1}' <<< "$ID_LIKE")
 
+if  [[ "$ID" = "debian" ]]; then 
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/debian.sh 
+	bash $FILES/debian.sh 
+	exit
+elif [[ "$ID" = "ubuntu" ]]; then
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/ubuntu.sh 
+	bash $FILES/ubuntu.sh 
+	exit
+elif [[ "$ID" = "arch" ]]; then
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/arch.sh 
+	bash $FILES/arch.sh 
+	exit
+elif [[ "$IDLIKE" = "debian" ]]; then 
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likedebian.sh 
+	bash $FILES/likedebian.sh 
+	exit
+elif [[ "$IDLIKE" = "ubuntu" ]]; then
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likeubuntu.sh 
+	bash $FILES/likeubuntu.sh 
+	exit
+else
+	echo Sowwy, not supported yet :3
+fi
+rm -rf /tmp/updater

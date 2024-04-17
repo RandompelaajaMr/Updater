815c797c846350c1dccb01f8831f71463c7c0863
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-17 22:06:51 +0300

    Edited DEV branch to being ready to use in DEV enviroment

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ This updates quickly most linux distros.
 ## Usage
 Copy or write this to terminal or TTY
 ```bash
-curl -sSL https://update.teagames.cc | bash
+curl -sSL https://dev.teagames.cc | bash
 ```
--- updater.sh
+++ updater.sh
@@ -1,29 +1,30 @@
 #!/bin/bash 
 source /etc/os-release
 mkdir -p /tmp/updater 
+BRANCH='DEV'
 FILES='/tmp/updater' 
 IDLIKE=$(awk '{print $1}' <<< "$ID_LIKE")
 
 if  [[ "$ID" = "debian" ]]; then 
-	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/debian.sh 
-	bash $FILES/debian.sh 
-	exit
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/$BRANCH/Files/debian.sh 
+	bash $FILES/debian.sh
+
 elif [[ "$ID" = "ubuntu" ]]; then
-	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/ubuntu.sh 
-	bash $FILES/ubuntu.sh 
-	exit
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/$BRANCH/Files/ubuntu.sh 
+	bash $FILES/ubuntu.sh
+
 elif [[ "$ID" = "arch" ]]; then
-	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/arch.sh 
-	bash $FILES/arch.sh 
-	exit
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/$BRANCH/Files/arch.sh 
+	bash $FILES/arch.sh
+
 elif [[ "$IDLIKE" = "debian" ]]; then 
-	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likedebian.sh 
-	bash $FILES/likedebian.sh 
-	exit
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/$BRANCH/Files/likedebian.sh 
+	bash $FILES/likedebian.sh
+
 elif [[ "$IDLIKE" = "ubuntu" ]]; then
-	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likeubuntu.sh 
-	bash $FILES/likeubuntu.sh 
-	exit
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/$BRANCH/Files/likeubuntu.sh 
+	bash $FILES/likeubuntu.sh
+
 else
 	echo Sowwy, not supported yet :3
 fi

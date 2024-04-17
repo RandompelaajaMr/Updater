375ddd5b60a0a1db68592769fbe26f6d4906c112
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-17 21:50:26 +0300

    Fixed things

--- updater.sh
+++ updater.sh
@@ -6,24 +6,24 @@ IDLIKE=$(awk '{print $1}' <<< "$ID_LIKE")
 
 if  [[ "$ID" = "debian" ]]; then 
 	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/debian.sh 
-	bash $FILES/debian.sh 
-	break
+	bash $FILES/debian.sh
+
 elif [[ "$ID" = "ubuntu" ]]; then
 	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/ubuntu.sh 
-	bash $FILES/ubuntu.sh 
-	break
+	bash $FILES/ubuntu.sh
+
 elif [[ "$ID" = "arch" ]]; then
 	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/arch.sh 
-	bash $FILES/arch.sh 
-	break
+	bash $FILES/arch.sh
+
 elif [[ "$IDLIKE" = "debian" ]]; then 
 	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likedebian.sh 
-	bash $FILES/likedebian.sh 
-	break
+	bash $FILES/likedebian.sh
+
 elif [[ "$IDLIKE" = "ubuntu" ]]; then
 	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/main/Files/likeubuntu.sh 
-	bash $FILES/likeubuntu.sh 
-	break
+	bash $FILES/likeubuntu.sh
+
 else
 	echo Sowwy, not supported yet :3
 fi

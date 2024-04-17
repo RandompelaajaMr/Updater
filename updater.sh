d7e54b2db76e527e6c04b4bd79a8ffdf2553a76b
RandompelaajaMr <99534959+RandompelaajaMr@users.noreply.github.com>
2024-04-17 20:40:13 +0300

    Adding/Modified files

--- updater.sh
+++ updater.sh
@@ -1,7 +1,25 @@
-#!/bin/bash
-mkdir -p /tmp/updater  
+#!/bin/bash 
+source /etc/os-release
+mkdir -p /tmp/updater 
 FILES='/tmp/updater' 
-wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/Files/ubuntu.sh 
-bash $FILES/ubuntu.sh 
-sudo rm -rf /tmp/updater 
-cd ~ || exit
+
+if  [[ "$ID" = "debian" ]]; then 
+        echo 'its DEBIAN'
+elif [[ "$ID" = "ubuntu" ]]; then
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/Files/ubuntu.sh 
+    bash $FILES/ubuntu.sh 
+	exit
+elif [[ "$ID" = "arch" ]]; then
+	echo 'its ARCH'
+elif [[ "$ID_LIKE" = "debian" ]]; then 
+	echo 'its based on DEBIAN'
+elif [[ "$ID_LIKE" = "ubuntu" ]]; then
+	wget -q -P $FILES https://raw.githubusercontent.com/RandompelaajaMr/Updater/DEV/Files/likeubuntu.sh 
+    bash $FILES/likeubuntu.sh 
+	exit
+elif [[ "$ID_LIKE" = "ubuntu debian" ]]; then
+	echo 'its based on UBUNTU AND DEBIAN'
+else
+	echo Sowwy, not supported yet :3
+fi
+rm -rf /tmp/updater
